551ee65088189133be90a96e8e38d865a9be9a75
MorrisKiman <morrisnjorogekimani@gmail.com>
2022-04-11 19:35:48 +0300

    New dir structure. Added arithmetics

---
+++ ArithmeticOps/task_tax_calc.py
@@ -0,0 +1,16 @@
+##Addition
+print("1 + 1= ",1+1)
+
+##Subtraction
+z=32-2
+print("32 - 2 = ",z)
+
+##multiplication
+print("12 x 12 = ", 12*12)
+
+##division
+z= 50/2
+print("50 / 2 = ", z)
+
+##modulus
+print ("50 / 3 = ", 50/3, "rem: ", 50%3)
