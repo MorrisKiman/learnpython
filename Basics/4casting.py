e399e6f4ce1fbe8fd3006ad1d8e2aa54b1697571
MorrisKiman <morrisnjorogekimani@gmail.com>
2022-04-14 12:20:25 +0300

    variables and variable types

---
+++ Basics/4casting.py
@@ -0,0 +1,18 @@
+x = str(3) #x is '3'
+y = int(3) # y is 3
+z = float(3) # y is 3.0
+
+##testing 123
+red = 23.1234
+yellow = 34.19
+blue = int(red + yellow)
+print('The value of blue is ', blue)
+print(type(red))
+print(type(yellow))
+print(type(x))
+
+#pint("John") is the same as print('John')
+#Python is case sensitive
+a = 12
+A = 'Don Joe'
+print(A,"has",a,'oranges and $',yellow,"/= in his pocket")
---
+++ Basics/5var_naming.py
@@ -0,0 +1,15 @@
+_name = "Flappy Bird"
+ID_number_ = 2345
+firstName_ = "Flappy"
+Other_Names = "Bird"
+eyeColor, height, _weight = "red", "180cm",'90kg'
+
+print('New student name is '+firstName_+' and full names are ', _name)
+print('Eye Color: ',eyeColor,"; height: ", height, "; weight: ", _weight)
+
+#collection of values
+fruits = ["mangoe", "banana", "cherry"]
+x, y, z = fruits
+print("fruit 1: "+x)
+print("fruit 2: "+y)
+print("fruit 3: "+z)
