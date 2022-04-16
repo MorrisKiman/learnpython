718b30b06575f70f9cf75b2f6b0a96d09b5c4a34
MorrisKiman <morrisnjorogekimani@gmail.com>
2022-04-16 17:52:56 +0300

    practice section added

---
+++ _practice/task1.py
@@ -0,0 +1,9 @@
+#create a program that takes in values for a list 'cars' with items x,y,z and print out x
+x = input("Enter first car: ")
+y=input("Enter second car: ")
+z=input("Enter third car: ")
+cars = [x,y,z]
+
+print("The car in value x = ",cars[0])
+print("The car in value x = ",cars[1])
+print("The car in value x = ",cars[2])
